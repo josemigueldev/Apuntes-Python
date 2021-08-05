7349f103ddc412c55e0e3fca3882aebd67e6cc4f
Jose Miguel <josemiguel.developer@gmail.com>
2021-08-04 23:48:58 -0500

    02 - variables y tipos de datos

---
+++ 02_variables_tipos.py
@@ -0,0 +1,25 @@
+"""
+VARIABLES Y TIPOS DE DATOS
+===============================================================================
+Una variable es un espacio para alamacenar datos modificables de forma temporal
+en la memoria del ordenador.
+
+Cada vaiable, tiene un nombre y un valor. Este ultimo define el tipo de dato
+de la variable.
+
+En python el concepto de constante es simbolico ya que todo espacio es variable
+"""
+CONSTANTE_PI = 3.1416
+print(CONSTANTE_PI)
+
+mi_cadena = "Hola Mundo!"
+numero_entero = 21
+numero_real = 35.05
+booleano = True
+
+print(
+    type(mi_cadena),
+    type(numero_entero),
+    type(numero_real),
+    type(booleano),
+)
